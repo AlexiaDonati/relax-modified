67d9dc9d87a137555f37a9b7ac0f674a0cb2c3e4
Alexia <donati.alexia@gmail.com>
2025-10-27 13:24:03 +0000

    Refactor EditorBagalg and EditorRelalg components to remove the display of the right arrow operator

--- src/calc2/components/editorRelalg.tsx
+++ src/calc2/components/editorRelalg.tsx
@@ -189,12 +189,14 @@ export class EditorRelalg extends React.Component<Props, State> {
 								tooltipTitle: 'calc.editors.ra.toolbar.rename-columns-operator',
 								tooltip: 'calc.editors.ra.toolbar.rename-columns-operator-content',
 							},
+							/*
 							{
 								label: '→',
 								onClick: this.replaceText,
 								tooltipTitle: 'calc.editors.ra.toolbar.right-arrow',
 								tooltip: 'calc.editors.ra.toolbar.right-arrow-content',
 							},
+							*/
 							{
 								label: 'τ',
 								onClick: this.replaceText,
